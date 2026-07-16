55457ba6d85ebef42b5e6bbea73439e18b42d8a3
ted <ted.elmenheim@iver.se>
2026-07-16 14:50:10 +0200

    fix levels

--- src/internal/game/level.go
+++ src/internal/game/level.go
@@ -3,8 +3,8 @@ package game
 import "flappy/internal/game/sprite"
 
 const (
-	LevelStartDads     = 1
-	LevelDadsIncrement = 1
+	LevelStartDads     = 7
+	LevelDadsIncrement = 6
 
 	BossHitsRequired  = 5
 	BossThrowsAllowed = 8
